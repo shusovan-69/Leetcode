475869af0eff545a322f3c69b7e682709ed1fa85
Shusovan Dutta <shusovandutta153@gmail.com>
2026-07-10 00:12:05 +0530

    Time: 106 ms (15.53%) | Memory: 9.5 MB (13.96%) - LeetSync

---
+++ 1-two-sum/two-sum.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    int i, j;
+
+    // Allocate memory for the answer (2 indices)
+    int *ans = (int *)malloc(2 * sizeof(int));
+
+    for (i = 0; i < numsSize - 1; i++)
+    {
+        for (j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                ans[0] = i;
+                ans[1] = j;
+                *returnSize = 2;
+                return ans;
+            }
+        }
+    }
+
+    *returnSize = 0;
+    return NULL;
+}
